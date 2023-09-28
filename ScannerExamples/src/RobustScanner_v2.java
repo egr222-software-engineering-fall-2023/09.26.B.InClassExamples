9a6cd48fdc27143f46b1263d17cf4cfdf0e9dfe1
Larry Clement <lclement@calbaptist.edu>
2023-09-27 19:01:55 -0700

    Prior to class on 09.28.2023

--- ScannerExamples/src/RobustScanner_v2.java
+++ ScannerExamples/src/RobustScanner_v2.java
@@ -4,14 +4,14 @@ public class RobustScanner_v2 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.print("How old are you? ");
-        boolean ageEntered = console.hasNextInt();
+        boolean ageEntered = console.hasNextInt();  // check the next token to see if it is an int
         while (!ageEntered) {
 //            console.next();   // discard the non-integer token...or...
             // get the non-integer token so we can provide an “intelligent” error message
             String invalidAge = console.next();   // get the non-integer token so we can have
             System.out.println(invalidAge + " is not a valid age.");
             System.out.print("How old are you? ");
-            ageEntered = console.hasNextInt();  // check the next token
+            ageEntered = console.hasNextInt();  // check the next token to see if it is an int
         }
         // if we get here, we know the user typed an integer
         int age = console.nextInt();
